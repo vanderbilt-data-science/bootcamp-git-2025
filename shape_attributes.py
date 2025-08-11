632517f494e6ebdee8748ffb21b3fc732564ca18
Myranda <36711263+myrandaGoesToSpace@users.noreply.github.com>
2025-08-11 15:25:58 -0500

    add shape file

---
+++ shape_attributes.py
@@ -0,0 +1,27 @@
+# DS 5380/5384 - Data Science Teamwork in Practice/Leading Data Science Teams
+# Practicing GitHub Flow
+
+## In this python file, you will calculate the area/surface area, diameter and circumference of a sphere and circle of radius of size r.
+## You can either set a value for pi or use the value from the math library
+r = 3
+import math
+## Define your functions to calculate the attributes below:
+
+### Area
+
+#### print your results here:
+#print(f"Area of circle with radius size {r} = {<name_of_function>(r):.2f}")
+#print(f"Area of sphere with radius size {r} = {<name_of_function>(r):.2f}")
+
+### Diameter
+
+
+#### print your results here:
+#print(f"Area of circle with radius size {r} = {<name_of_function>(r):.2f}")
+#print(f"Area of sphere with radius size {r} = {<name_of_function>(r):.2f}")
+
+### Circumference
+
+#### print your results here:
+#print(f"Area of circle with radius size {r} = {<name_of_function>(r):.2f}")
+#print(f"Area of sphere with radius size {r} = {<name_of_function>(r):.2f}")
